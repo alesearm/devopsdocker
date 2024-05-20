b21cc19aafdb8a9eb7d876028393e1d65a2aff3d
alesearm <alesearm@students.zhaw.ch>
2024-05-20 19:34:02 +0200

    server.js anpassung

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-	res.send('Hello FS2024 DevOps Course! :-))) mY nAME IS aRMIN(((-:');
+	res.send('Hello FS2024 DevOps Course! :-))) Hallo Zusammen. Ich heisse Armin (((-:');
 });
 
 app.listen(PORT, HOST);
